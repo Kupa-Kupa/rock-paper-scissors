6425b6f45b10e1aaf7d60846c9374cdcea197371
cooper <cooper.elliott30@gmail.com>
2022-11-15 12:29:46 +1100

    Add scripts and index file

---
+++ scripts/rock-paper-scissors.js
@@ -0,0 +1,42 @@
+// Rock Paper Scissors
+
+const computerSelection = getComputerChoice();
+
+const playerSelection = getPlayerSelection();
+
+
+// computer gets choice of rock, paper, or scissors
+function getComputerChoice(){
+    
+
+}
+
+
+
+// prompt user for their choice
+
+function getPlayerSelection(){
+
+}
+
+// compare answers and determine winner
+
+
+
+// play
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection) {
+        console.log(``)
+    }
+
+}
+
+console.log(playRound());
+
+
+// Play 5 round game - could pass a const of round into game if I wanted
+
+function game() {
+
+}
